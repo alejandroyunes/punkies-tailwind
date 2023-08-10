5c992e7fd5f1af8c6e494f89e74d1d1a8f54bbcb
Alejandro Yunes <alejandroyunes@outlook.com>
2023-08-10 00:06:08 -0500

    added theme provider and nav bar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,32 @@
 import './globals.css'
-import ThemeProvider from './modules/themeProvider'
+import { ThemeProvider } from './context/themeProvider'
 import { Metadata } from 'next'
+import { Inter } from "next/font/google";
+import { ThemeSwitcher } from './components/ThemeSwitcher';
+import Navbar from './components/Navbar';
 
-const metadata: Metadata = {
-  title: 'Punkies y Cerebro'
-}
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Punkies y Cerebro',
+    template: 'punk rock de la ciudad de Medellín',
+  },
+  description:
+    'Banda activa de punk rock',
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+
   return (
     <html lang="en">
-      <body>
-        <ThemeProvider>
-          {children}
-        </ThemeProvider>
-      </body>
+        <body className={`${inter.className} bg-slate-50 dark:bg-[#0d1117]`}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <Navbar />
+            <main>{children}</main>
+          </ThemeProvider>
+        </body>
     </html>
   )
 }
